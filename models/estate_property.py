4f901257dde87e772113aa40473af04bd2849696
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-09-02 21:01:17 +0800

    learn and add more fields definitions

---
+++ models/estate_property.py
@@ -0,0 +1,27 @@
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "test property of real estate"
+
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date('Available Date')
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer('Living Area (sqm)')
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer('Garden Size (sqm)')
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
--- models/test_model.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class TestModel(models.Model):
-    _name = "test.model"
-    _description = "test model of real estate"
-
-    name = fields.Char()
