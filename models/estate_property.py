cbc18b5904d095998f363213f8ef51be0803dd7a
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-09-03 17:24:26 +0800

    learn how to add attributes to fields

--- models/estate_property.py
+++ models/estate_property.py
@@ -5,18 +5,18 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "test property of real estate"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date('Available Date')
+    date_availability = fields.Date("Available Date")
     expected_price = fields.Float()
-    selling_price = fields.Float()
+    selling_price = fields.Float(required=True)
     bedrooms = fields.Integer()
-    living_area = fields.Integer('Living Area (sqm)')
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer('Garden Size (sqm)')
+    garden_area = fields.Integer("Garden Size (sqm)")
     garden_orientation = fields.Selection(
         selection=[
             ("north", "North"),
