f31ed1b6466646a4fb91b53f8f6720e75f369b32
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-09-04 15:02:23 +0800

    learn how to add action

--- __manifest__.py
+++ __manifest__.py
@@ -2,5 +2,8 @@
     "name": "Real Estate",
     "depends": ["base"],
     "application": True,
-    "data": ["security/ir.model.access.csv"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+    ],
 }
