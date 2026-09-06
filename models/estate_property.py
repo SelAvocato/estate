4a194ed2f74eed9a0ca236b804b0bed9ce677f59
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-09-06 18:46:27 +0800

    add the real estate proper offer table

--- models/estate_property.py
+++ models/estate_property.py
@@ -41,6 +41,7 @@ class EstateProperty(models.Model):
         ],
         default="new",
     )
-    salesperson_id = fields.Many2one('res.users',default=lambda self: self.env.user)
-    buyer_id = fields.Many2one('res.partner', copy=False)
-    tag_ids = fields.Many2many('estate.property.tag')
+    salesperson_id = fields.Many2one("res.users", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", copy=False)
+    tag_ids = fields.Many2many("estate.property.tag")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
