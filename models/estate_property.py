b29639e0b845de2fa31e0d0bcac92177d0a5af92
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-09-06 19:32:51 +0800

    learn how to use depends decorator and compute the total area

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -45,3 +45,9 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner", copy=False)
     tag_ids = fields.Many2many("estate.property.tag")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    total_area = fields.Integer(compute="_compute_total")
+
+    @api.depends("garden_area", "living_area")
+    def _compute_total(self):
+        for record in self:
+            record.total_area = record.garden_area + record.living_area
